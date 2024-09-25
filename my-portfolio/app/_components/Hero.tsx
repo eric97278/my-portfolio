d393740bd9655ed18deae952730fe719cefcdd04
eric97278 <159042489+eric97278@users.noreply.github.com>
2024-09-25 15:28:56 +0200

    Ajout de Lucide icon plus de la partie projets,ajustement des paddings

--- my-portfolio/app/_components/Hero.tsx
+++ my-portfolio/app/_components/Hero.tsx
@@ -16,7 +16,7 @@ const Code = ({ className, ...props }: ComponentPropsWithoutRef<"span">) => {
 
 export const Hero = () => {
    return <Section className="flex max-md:flex-col items-start gap-4">
-      <div className="flex-[2] flex flex-col gap-2">
+      <div className="flex-[3] flex flex-col gap-2">
          <h2 className="font-caption font-bold text-5xl text-primary">Chillan Eric</h2>
          <h3 className="text-3xl font-caption">
             Developpeur Full-Stack en recherche alternance</h3>
@@ -30,7 +30,7 @@ export const Hero = () => {
             </Link>
          </p>
       </div>
-      <div className="flex-1 max-md:m-auto ml-auto">
+      <div className="flex-[2] max-md:m-auto ml-auto">
          <img
             src= "https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_1280.jpg"
             className="w-full h-auto max-w-xs max-w-xs max-md:w-56 "
--- my-portfolio/app/_components/Status.tsx
+++ my-portfolio/app/_components/Status.tsx
@@ -1,9 +1,86 @@
-import { Section } from "./Section"
+import { Card } from "@/components/ui/card";
+import { Section } from "./Section";
+import { LucideIcon, Smartphone, Scroll, Calculator, Sun } from 'lucide-react';
+import Link from "next/link";
 
 export const Status = () => {
-   return <Section>
-      <div>
-         
-      </div>
-   </Section>
+   return (
+      <Section className="flex max-md:flex-col items-start gap-4">
+         <div className="flex-[3] w-full">
+            <Card className=" p-4 w-full flex flex-coll gap-2 w-ful">
+               <p className="text-lg text-muted-foreground">Side, fun projects</p>
+               <div className="flex flex-col gap-4">
+                  {SIDE_PROJECTS.map((project, index) => (
+                     <SideProject
+                        key={index}
+                        Logo={project.Logo}
+                        title={project.title}
+                        description={project.description}
+                        url="/"
+                     />
+                  ))}
+               </div>
+            </Card>
+         </div>
+         <div className="flex-[2] w-full flex flex-col gap-4">
+            <Card className="p-4 flex-1">Work</Card>
+            <Card className="p-4 flex-1">
+               Contact me
+            </Card>
+         </div>
+      </Section>
+   );
+};
+
+
+const SIDE_PROJECTS = [
+   {
+      Logo: Smartphone,
+      title: "Cook-your-way",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, ratione",
+      url: "Lorem ipsum dolor sit amet consectetur adipisicing."
+   },
+   {
+      Logo: Scroll,
+      title: "To-Do-List",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, ratione",
+      url: "Lorem ipsum dolor sit amet consectetur adipisicing."
+   },
+   {
+      Logo: Calculator,
+      title: "Calculator",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, ratione",
+      url: "Lorem ipsum dolor sit amet consectetur adipisicing."
+   },
+   {
+      Logo: Sun,
+      title: "Projet-météo",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, ratione",
+      url: "Lorem ipsum dolor sit amet consectetur adipisicing."
+   }
+]
+
+type SideProjectProps = {
+   Logo: LucideIcon;
+   title: string;
+   description: string;
+   url: string;
+}
+
+const SideProject = (props: {
+   
+   Logo: LucideIcon, title: string, description: string, url: string;
+}) => {
+   return (
+      <Link href={props.url} className="inline-flex items-center gap-4 hover-bg-accent/50 transition-colors p-1 rounded">
+      
+         <span className="bg-accent text-accent-foreground p-3 rounded-sm">
+            <props.Logo />
+         </span>
+         <div>
+            <p className="text-lg font-semibold">{props.title}</p>
+            <p className="text-sm text-muted-foreground">{props.description}</p>
+         </div>
+      </Link>
+   );
 }
