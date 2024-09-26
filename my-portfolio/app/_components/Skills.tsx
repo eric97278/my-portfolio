b38310a8fa9dec73d9aaa7dab043112c1219192f
eric97278 <159042489+eric97278@users.noreply.github.com>
2024-09-26 18:07:00 +0200

    ajout des stacks et icones

--- my-portfolio/app/_components/Skills.tsx
+++ my-portfolio/app/_components/Skills.tsx
@@ -1,7 +1,12 @@
 import { Code } from "./Code";
+import { AndroidStudioIcon } from "./icons/AndroidStudioLogo";
 import { CssLogo } from "./icons/CssLogo";
 import { HtmlLogo } from "./icons/HtmlLogo";
 import { JavascriptLogo } from "./icons/JavascriptLogo";
+import { KotlinLogo } from "./icons/KotlinLogo";
+import { LaravelLogo } from "./icons/LaravelLogo";
+import { MysqlLogo } from "./icons/MysqlLogo";
+import { PhpLogo } from "./icons/PhpLogo";
 import { ReactLogo } from "./icons/ReactLogo";
 import { TailswindLogo } from "./icons/TailswindLogo";
 import { Section } from "./Section";
@@ -22,7 +27,8 @@ export const Skills = () => {
                   style={{ animationDuration: "10s" }} /> 
                <h3 className="text-2xl font-semibold tracking-tight mb-2 ">React</h3>
                <p className="text-sm text-muted-foreground ">My main framework is <Code>React</Code>. I also <Code>Next.js</Code> {"as a backend and frontend framework."}</p>
-            </div> <div className="flex flex-col gap-2">
+            </div>
+            <div className="flex flex-col gap-2">
                <TailswindLogo
                   size={42}
                />
@@ -39,21 +45,40 @@ export const Skills = () => {
                   />
                   <CssLogo
                   size={42}
-                  />
-                  
+                  /> 
                </div>
                <h3 className="text-2xl font-semibold tracking-tight mb-2 ">HTML / JAVASCRIPT / CSS</h3>
                <p className="text-sm text-muted-foreground ">J'ai débuté les bases en <Code>HTML</Code>,<Code>CSS
                </Code>  et<Code>JavaScript</Code>  pour créer des interfaces web dynamiques et responsives</p>
-            </div> <div className="flex flex-col gap-2">
-               
-               <h3 className="text-2xl font-semibold tracking-tight mb-2 ">Marketing</h3>
-               <p className="text-sm text-muted-foreground ">My main framework is <Code>React</Code>. I also <Code>Next.js</Code> {"as a backend and frontend framework."}</p>
-            </div> <div className="flex flex-col gap-2">
-               
-               
+            </div> 
+            <div className="flex flex-col gap-2">
+            <div className="flex gap-4">
+               <PhpLogo
+                  size={42}
+               />
+               <LaravelLogo
+                  size={42}
+               />
+               <MysqlLogo
+                  size={42}
+                  /> 
+               </div>
+               <h3 className="text-2xl font-semibold tracking-tight mb-2 ">PHP / LARAVEL / MYSQL</h3>
+               <p className="text-sm text-muted-foreground ">Lors de ma formation chez <Code>Ada Tech School</Code>, j'ai fais des projet en <Code>Php</Code>avec son framework <Code>Laravel</Code>  en utilisant comme base de données <Code>Mysql</Code> .</p>
             </div>
-         </div>
+            <div className="flex flex-col gap-2">
+               <div className="flex gap-4">
+                  <KotlinLogo
+                     size={42}
+                  />
+                  <AndroidStudioIcon
+                     size={42}
+                  />
+               </div>
+               <h3 className="text-2xl font-semibold tracking-tight mb-2 ">Kotlin / Android studio</h3>
+               <p className="text-sm text-muted-foreground ">j'adore utiliser le langage <Code>Kotlin</Code>avec l'IDE de <Code>Android studio</Code> mon premier projet avec cette techno est, une application de cuisine <Code>Cook-your-way</Code> .</p>
+            </div>
+            </div>
       </Section>
    );
 };
